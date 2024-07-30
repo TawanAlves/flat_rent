1dbce3f4b32c03cd6f5140ce23eb6bac6a6c53b5
tawan <tawan@flyskyler.com>
2024-07-30 17:24:58 -0300

    first commit

---
+++ core/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+import core.models as models
+
+admin.site.register(models.Flat)
---
+++ core/models/__init__.py
@@ -0,0 +1,6 @@
+"""
+    Models
+    Centralizes the importing of all the apps modules.
+"""
+
+from .models_flat import Flat
