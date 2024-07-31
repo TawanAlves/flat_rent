2dde33b1416ac9258a0ccb60c127a84c5733e51c
tawan <tawan@flyskyler.com>
2024-07-31 13:51:06 -0300

    creat main view

--- flatrent/core/models/models_booking.py
+++ flatrent/core/models/models_booking.py
@@ -4,8 +4,8 @@ from django.db import models
 class Booking(models.Model):
     flat = models.ForeignKey(
         'Flat', on_delete=models.CASCADE, related_name='booking')
-    checkin = models.DateTimeField(null=True, blank=True)
-    checkout = models.DateTimeField(null=True, blank=True)
+    checkin = models.DateField(null=True, blank=True)
+    checkout = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.id} - {self. flat}'
