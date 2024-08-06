79bbe750ee414cffec9770fe56f81659746cfc48
tawan <tawan@flyskyler.com>
2024-08-06 00:04:38 -0300

    feat: frontend style configs

---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <HomeViews />
+</template>
+
+<script setup>
+//
+</script>
--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -33,6 +33,13 @@ export default defineConfig({
       },
     }),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {        
+        additionalData: `@import "@/styles/settings.scss";`
+      }
+    }
+  },
   define: { 'process.env': {} },
   resolve: {
     alias: {
