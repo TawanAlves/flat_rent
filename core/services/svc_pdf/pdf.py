00f0e23c1b5b7ece50c613e2f4527ffe0d9ceb4d
tawan <tawan@flyskyler.com>
2024-08-06 00:08:32 -0300

    feat: Send email and Create PDF Script

--- core/services/svc_pdf/pdf.py
+++ core/services/svc_pdf/pdf.py
@@ -5,32 +5,6 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
-import config
-
-
-def send_email(subject, body, to_email):
-    from_email = config.EMAIL
-    password = config.PASSWORD
-    # to_email = config.TOEMAIL
-
-    msg = MIMEMultipart()
-    msg['From'] = from_email
-    msg['To'] = to_email
-    msg['Subject'] = subject
-
-    msg.attach(MIMEText(body, 'plain'))
-
-    with open('./output/meu_arquivo.pdf', 'rb') as file:
-        part = MIMEApplication(file.read(), Name='attachment.pdf')
-        part['Content-Disposition'] = f'attachment; filename="attachment.pdf"'
-        msg.attach(part)
-
-    with smtplib.SMTP('smtp.gmail.com', 587) as server:
-        server.starttls()
-        server.login(from_email, password)
-        server.send_message(msg)
-
-    print('Email with PDF sent successfully!')
 
 
 class PDF:
@@ -38,7 +12,67 @@ class PDF:
         self.environment = jinja2.Environment(
             loader=jinja2.FileSystemLoader(searchpath='./'))
 
-    def create_pdf(self, data: dict, template_file: str, output_file: str, css_file: str) -> None:
+    def send_email(self, subject, body, to_email):
+        from_email = 'your email'
+        password = 'your password'
+
+        msg = MIMEMultipart()
+        msg['From'] = from_email
+        msg['To'] = to_email
+        msg['Subject'] = subject
+
+        msg.attach(MIMEText(body, 'plain'))
+
+        output_file = os.path.abspath(os.path.join(
+            os.path.dirname(__file__), 'output/meu_arquivo.pdf'))
+        with open(output_file, 'rb') as file:
+            part = MIMEApplication(file.read(), Name='attachment.pdf')
+            part['Content-Disposition'] = f'attachment; filename="attachment.pdf"'
+            msg.attach(part)
+
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()
+            server.login(from_email, password)
+            server.send_message(msg)
+
+        print('Email with PDF sent successfully!')
+
+        if os.path.exists(output_file):
+            os.remove(output_file)
+            print("File deleted successfully!")
+
+    def create_pdf(self, data: dict) -> None:
+        base_dir = os.path.dirname(__file__)
+        input_folder = os.path.abspath(os.path.join(base_dir, 'input'))
+        output_folder = os.path.abspath(os.path.join(base_dir, 'output'))
+        os.makedirs(output_folder, exist_ok=True)
+
+        template_file = os.path.abspath(
+            os.path.join(input_folder, 'index.html'))
+        output_file = os.path.abspath(
+            os.path.join(output_folder, 'meu_arquivo.pdf'))
+        css_file = os.path.abspath(os.path.join(input_folder, 'style.css'))
+
+        print(f"Base directory: {base_dir}")
+        print(f"Template file path: {template_file}")
+        print(f"CSS file path: {css_file}")
+        print(f"Output file path: {output_file}")
+
+        if not os.path.exists(template_file):
+            raise FileNotFoundError(
+                f"Template file '{template_file}' does not exist.")
+        if not os.path.exists(css_file):
+            raise FileNotFoundError(f"CSS file '{css_file}' does not exist.")
+        print(f"Template file path: {template_file}")
+        print(f"CSS file path: {css_file}")
+        print(f"Output file path: {output_file}")
+
+        if not os.path.exists(template_file):
+            raise FileNotFoundError(
+                f"Template file '{template_file}' does not exist.")
+        if not os.path.exists(css_file):
+            raise FileNotFoundError(f"CSS file '{css_file}' does not exist.")
+
         with open(template_file, 'r') as f:
             template = f.read()
         template = self._populate_template(data, template, css_file)
@@ -64,30 +98,3 @@ class PDF:
             css_content = f.read()
         data_with_css = {**data, 'style': css_content}
         return jinja_template.render(**data_with_css)
-   #   print(f"Ocorreu um erro: {e}")
-
-
-pdf = PDF()
-try:
-    input_folder = os.path.join(os.getcwd(), 'input')
-    output_folder = os.path.join(os.getcwd(), 'output')
-    os.makedirs(output_folder, exist_ok=True)
-    css_file = os.path.join(input_folder, 'style.css')
-
-    pdf.create_pdf(
-        data={},
-        template_file='./input/index.html',
-        output_file='./output/meu_arquivo.pdf',
-        css_file=css_file
-    )
-
-    with open('./output/meu_arquivo.pdf', 'rb') as f:
-        print("O arquivo foi gerado com sucesso!")
-        send_email('Test Subject', 'Test Body')
-except FileNotFoundError:
-    print("Arquivo não encontrado.")
-except Exception as e:
-    print(f"Ocorreu um erro: {e}")
-
-
-# send_email('Test Subject', 'Test Body', 'CV.pdf')
